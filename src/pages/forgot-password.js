944dad88a02ee66716dc21d946d5337452984355
Blede <maxtergameplay@gmail.com>
2022-09-08 22:58:37 -0400

    Forgot password page

--- src/pages/forgot-password.js
+++ src/pages/forgot-password.js
@@ -1,14 +1,12 @@
 import ApplicationLogo from '@/components/ApplicationLogo'
 import AuthCard from '@/components/AuthCard'
 import AuthSessionStatus from '@/components/AuthSessionStatus'
-import Button from '@/components/Button'
 import GuestLayout from '@/components/Layouts/GuestLayout'
-import Input from '@/components/Input'
 import InputError from '@/components/InputError'
-import Label from '@/components/Label'
 import Link from 'next/link'
 import { useAuth } from '@/hooks/auth'
 import { useState } from 'react'
+import { Box, Button, Text, TextInput, Title } from '@mantine/core'
 
 const ForgotPassword = () => {
     const { forgotPassword } = useAuth({ middleware: 'guest' })
@@ -29,41 +27,45 @@ const ForgotPassword = () => {
                 logo={
                     <Link href="/">
                         <a>
-                            <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                            <ApplicationLogo width={120} height={120} />
                         </a>
                     </Link>
                 }>
-
-                <div className="mb-4 text-sm text-gray-600">
-                    Forgot your password? No problem. Just let us know your
-                    email address and we will email you a password reset link
-                    that will allow you to choose a new one.
-                </div>
+                <Box mb={25}>
+                    <Title align="center" order={1} size={26}>
+                        Forgot your password?
+                    </Title>
+                    <Text color="dimmed" size="sm" align="center">
+                        Enter your email to get a reset link
+                    </Text>
+                </Box>
 
                 {/* Session Status */}
                 <AuthSessionStatus className="mb-4" status={status} />
 
                 <form onSubmit={submitForm}>
                     {/* Email Address */}
-                    <div>
-                        <Label htmlFor="email">Email</Label>
-                        <Input
-                            id="email"
-                            type="email"
-                            name="email"
+                    <Box>
+                        <TextInput
+                            placeholder="Your email"
                             value={email}
-                            className="block mt-1 w-full"
                             onChange={event => setEmail(event.target.value)}
-                            required
-                            autoFocus
+                            label="Email"
+                            withAsterisk
                         />
-
-                        <InputError messages={errors.email} className="mt-2" />
-                    </div>
-
-                    <div className="flex items-center justify-end mt-4">
-                        <Button>Email Password Reset Link</Button>
-                    </div>
+                        <InputError messages={errors.email} />
+                    </Box>
+                    <Box mt={10}>
+                        <Button
+                            fullWidth
+                            type="submit"
+                            variant="gradient"
+                            mt={5}
+                            sx={{ height: 45 }}
+                            gradient={{ from: 'indigo', to: 'violet' }}>
+                            Email Password Reset Link
+                        </Button>
+                    </Box>
                 </form>
             </AuthCard>
         </GuestLayout>
