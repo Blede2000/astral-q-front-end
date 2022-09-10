d69a1e0115e0be95922f609f7c06650c85334845
Blede <maxtergameplay@gmail.com>
2022-09-10 18:03:51 -0400

    Updated password reset page

--- src/pages/password-reset/[token].js
+++ src/pages/password-reset/[token].js
@@ -1,15 +1,13 @@
 import ApplicationLogo from '@/components/Utils/ApplicationLogo'
 import AuthCard from '@/components/Cards/AuthCard'
 import AuthSessionStatus from '@/components/AuthSessionStatus'
-import Button from '@/components/Button'
 import GuestLayout from '@/components/Layouts/GuestLayout'
-import Input from '@/components/Input'
 import InputError from '@/components/Forms/InputError'
-import Label from '@/components/Label'
 import Link from 'next/link'
 import { useAuth } from '@/hooks/auth'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import { Box, Button, PasswordInput, TextInput } from '@mantine/core'
 
 const PasswordReset = () => {
     const router = useRouter()
@@ -44,7 +42,7 @@ const PasswordReset = () => {
                 logo={
                     <Link href="/">
                         <a>
-                            <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                            <ApplicationLogo width={120} height={120} />
                         </a>
                     </Link>
                 }>
@@ -53,66 +51,53 @@ const PasswordReset = () => {
 
                 <form onSubmit={submitForm}>
                     {/* Email Address */}
-                    <div>
-                        <Label htmlFor="email">Email</Label>
-
-                        <Input
-                            id="email"
-                            type="email"
+                    <Box>
+                        <TextInput
+                            placeholder="Your email"
                             value={email}
-                            className="block mt-1 w-full"
                             onChange={event => setEmail(event.target.value)}
-                            required
-                            autoFocus
+                            label="Email"
+                            withAsterisk
                         />
-
-                        <InputError messages={errors.email} className="mt-2" />
-                    </div>
+                        <InputError messages={errors.email} />
+                    </Box>
 
                     {/* Password */}
-                    <div className="mt-4">
-                        <Label htmlFor="password">Password</Label>
-                        <Input
-                            id="password"
-                            type="password"
+                    <Box mt={10}>
+                        <PasswordInput
+                            placeholder="Password"
                             value={password}
-                            className="block mt-1 w-full"
                             onChange={event => setPassword(event.target.value)}
-                            required
+                            label="Password"
+                            autoComplete="new-password"
+                            withAsterisk
                         />
+                        <InputError messages={errors.password} />
+                    </Box>
 
-                        <InputError
-                            messages={errors.password}
-                            className="mt-2"
-                        />
-                    </div>
-
-                    {/* Confirm Password */}
-                    <div className="mt-4">
-                        <Label htmlFor="passwordConfirmation">
-                            Confirm Password
-                        </Label>
-
-                        <Input
-                            id="passwordConfirmation"
-                            type="password"
+                    <Box mt={10}>
+                        <PasswordInput
+                            placeholder="Confirm Password"
                             value={passwordConfirmation}
-                            className="block mt-1 w-full"
                             onChange={event =>
                                 setPasswordConfirmation(event.target.value)
                             }
-                            required
+                            label="Confirm Password"
+                            withAsterisk
                         />
+                        <InputError messages={errors.password_confirmation} />
+                    </Box>
 
-                        <InputError
-                            messages={errors.password_confirmation}
-                            className="mt-2"
-                        />
-                    </div>
-
-                    <div className="flex items-center justify-end mt-4">
-                        <Button>Reset Password</Button>
-                    </div>
+                    <Box mt="sm">
+                        <Button
+                            fullWidth
+                            type="submit"
+                            variant="gradient"
+                            sx={{ height: 45 }}
+                            gradient={{ from: 'indigo', to: 'violet' }}>
+                            Sign in
+                        </Button>
+                    </Box>
                 </form>
             </AuthCard>
         </GuestLayout>
